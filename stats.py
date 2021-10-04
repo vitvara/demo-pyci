e233d75b1f61a4ac17ff905a68bd8552df51070b
vitvara <gridxlgm@gmail.com>
2021-10-04 14:42:24 +0700

    Update stat.py

--- stats.py
+++ stats.py
@@ -30,7 +30,7 @@ def variance(data):
     # some deliberately misformatted code. Use flake8 to fix.
     n=len(data)  
     if n==0:
-        raise TypeError( )
+        raise ValueError( )
     average = sum(data)/n
     return sum( [(x-average)**2 for x in data] )/n
 
